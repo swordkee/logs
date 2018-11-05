3a1823964d41b761dac374d427bba92b09481ecc
swordkee <swordkee@futurebots.cn>
2018-11-05 16:36:21 +0800

    zap

--- logs.go
+++ logs.go
@@ -1,299 +1,352 @@
 package logs
 
 import (
-	"github.com/pkg/errors"
+	"bytes"
+	"fmt"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"log"
+	"runtime"
+	"strings"
+	"time"
 )
 
-// Logger is an interface that describes logging.
 type Logger interface {
-	SetLevel(level zapcore.Level)
-
 	Debug(...interface{})
 	Debugf(format string, args ...interface{})
-	Debugw(msg string, keysAndValues ...interface{})
+	Debugln(...interface{})
 
 	Info(...interface{})
 	Infof(format string, args ...interface{})
-	Infow(msg string, keysAndValues ...interface{})
+	Infoln(...interface{})
 
 	Warn(...interface{})
 	Warnf(format string, args ...interface{})
-	Warnw(msg string, keysAndValues ...interface{})
+	Warnln(...interface{})
 
 	Error(...interface{})
 	Errorf(format string, args ...interface{})
-	Errorw(msg string, keysAndValues ...interface{})
-
-	DPanic(...interface{})
-	DPanicf(format string, args ...interface{})
+	Errorln(...interface{})
 
 	Panic(...interface{})
 	Panicf(format string, args ...interface{})
+	Panicln(...interface{})
 
 	Fatal(...interface{})
 	Fatalf(format string, args ...interface{})
+	Fatalln(...interface{})
 
-	With(...interface{}) *ZapLogger
+	With(key string, value interface{}) *ZapLogger
 }
-type ProcessorFunc func() zapcore.Field
+
+// callerEncoder will add caller to log. format is "filename:lineNum:funcName", e.g:"logs/logs_test.go:15:logs.TestNew"
+func callerEncoder(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(strings.Join([]string{caller.TrimmedPath(), runtime.FuncForPC(caller.PC).Name()}, ":"))
+}
+
+// timeEncoder specifics the time format
+func timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05"))
+}
+
+// milliSecondsDurationEncoder serializes a time.Duration to a floating-point number of micro seconds elapsed.
+func milliSecondsDurationEncoder(d time.Duration, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendFloat64(float64(d) / float64(time.Millisecond))
+}
+
+func newLoggerConfig(debugLevel bool, te zapcore.TimeEncoder, de zapcore.DurationEncoder) (loggerConfig zap.Config) {
+	loggerConfig = zap.NewProductionConfig()
+	if te == nil {
+		loggerConfig.EncoderConfig.EncodeTime = timeEncoder
+	} else {
+		loggerConfig.EncoderConfig.EncodeTime = te
+	}
+	if de == nil {
+		loggerConfig.EncoderConfig.EncodeDuration = milliSecondsDurationEncoder
+	} else {
+		loggerConfig.EncoderConfig.EncodeDuration = de
+	}
+	loggerConfig.EncoderConfig.EncodeCaller = callerEncoder
+	if debugLevel {
+		loggerConfig.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	}
+	return
+}
+
+// NewLogger return a normal logger
+func NewLogger(debugLevel bool) (logger *zap.Logger) {
+	loggerConfig := newLoggerConfig(debugLevel, nil, nil)
+	loggerConfig.DisableStacktrace = true
+	logger, err := loggerConfig.Build()
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+// NewNoCallerLogger return a no caller key value, will be faster
+func NewNoCallerLogger(debugLevel bool) (noCallerLogger *zap.Logger) {
+	loggerConfig := newLoggerConfig(debugLevel, nil, nil)
+	loggerConfig.DisableCaller = true
+	noCallerLogger, err := loggerConfig.Build()
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+// CompatibleLogger is a logger which compatible to logrus/std log/prometheus.
+// it implements Print() Println() Printf() Dbug() Debugln() Debugf() Info() Infoln() Infof() Warn() Warnln() Warnf()
+// Error() Errorln() Errorf() Fatal() Fataln() Fatalf() Panic() Panicln() Panicf() With() WithField() WithFields()
 
 type ZapLogger struct {
-	logger     *zap.Logger
-	Config     zap.Config
-	Level      zap.AtomicLevel
-	Processors []ProcessorFunc
+	_log *zap.Logger
 }
 
-func NewLogger() (*ZapLogger, error) {
-	a := zap.NewAtomicLevelAt(zap.InfoLevel)
-	l := &ZapLogger{
-		Config: zap.Config{
-			Development:       true,
-			DisableStacktrace: true,
-			EncoderConfig:     zap.NewDevelopmentEncoderConfig(),
-			Encoding:          "json",
-			ErrorOutputPaths:  []string{"stderr", "error.log"},
-			Level:             a,
-			OutputPaths:       []string{"stdout"},
-		},
-		Level: a,
-	}
-
-	logger, err := l.Config.Build()
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to build logger")
-	}
-	defer logger.Sync()
-	l.logger = logger
-	return l, nil
+// NewCompatibleLogger return CompatibleLogger with caller field
+func NewCompatibleLogger(debugLevel bool) *ZapLogger {
+	return &ZapLogger{NewLogger(debugLevel).WithOptions(zap.AddCallerSkip(1))}
 }
 
-// With attaches a fields pair to a logger.
-//func (l *ZapLogger) With(args ...interface{}) *zap.SugaredLogger {
-//	return l.logger.Sugar().With(args...)
-//}
-func (l *ZapLogger) With(fields ...zapcore.Field) *ZapLogger {
-	n := &ZapLogger{
-		logger:     l.logger.With(fields...),
-		Config:     l.Config,
-		Level:      l.Level,
-		Processors: make([]ProcessorFunc, len(l.Processors)),
-	}
-
-	for i, p := range l.Processors {
-		n.Processors[i] = p
-	}
-	return n
+// Debug logs a message at level Debug on the compatibleLogger.
+func (l ZapLogger) Debug(args ...interface{}) {
+	l._log.Debug(fmt.Sprint(args...))
 }
 
-// SetLevel sets the level of a logger.
-func (l *ZapLogger) SetLevel(level zapcore.Level) {
-	l.Level.SetLevel(level)
+// Debugln logs a message at level Debug on the compatibleLogger.
+func (l ZapLogger) Debugln(args ...interface{}) {
+	l._log.Debug(fmt.Sprint(args...))
 }
 
-//// SetOut sets the output destination for a logger.
-//func (l *ZapLogger) SetOut(out io.Writer) {
+// Debugf logs a message at level Debug on the compatibleLogger.
+func (l ZapLogger) Debugf(format string, args ...interface{}) {
+	l._log.Debug(fmt.Sprintf(format, args...))
+}
+
+// Info logs a message at level Info on the compatibleLogger.
+func (l ZapLogger) Info(args ...interface{}) {
+	l._log.Info(fmt.Sprint(args...))
+}
+
+// Infoln logs a message at level Info on the compatibleLogger.
+func (l ZapLogger) Infoln(args ...interface{}) {
+	l._log.Info(fmt.Sprint(args...))
+}
+
+// Infof logs a message at level Info on the compatibleLogger.
+func (l ZapLogger) Infof(format string, args ...interface{}) {
+	l._log.Info(fmt.Sprintf(format, args...))
+}
+
+// Warn logs a message at level Warn on the compatibleLogger.
+func (l ZapLogger) Warn(args ...interface{}) {
+	l._log.Warn(fmt.Sprint(args...))
+}
+
+// Warnln logs a message at level Warn on the compatibleLogger.
+func (l ZapLogger) Warnln(args ...interface{}) {
+	l._log.Warn(fmt.Sprint(args...))
+}
+
+// Warnf logs a message at level Warn on the compatibleLogger.
+func (l ZapLogger) Warnf(format string, args ...interface{}) {
+	l._log.Warn(fmt.Sprintf(format, args...))
+}
+
+// Error logs a message at level Error on the compatibleLogger.
+func (l ZapLogger) Error(args ...interface{}) {
+	l._log.Error(fmt.Sprint(args...))
+}
+
+// Errorln logs a message at level Error on the compatibleLogger.
+func (l ZapLogger) Errorln(args ...interface{}) {
+	l._log.Error(fmt.Sprint(args...))
+}
+
+// Errorf logs a message at level Error on the compatibleLogger.
+func (l ZapLogger) Errorf(format string, args ...interface{}) {
+	l._log.Error(fmt.Sprintf(format, args...))
+}
+
+// Fatal logs a message at level Fatal on the compatibleLogger.
+func (l ZapLogger) Fatal(args ...interface{}) {
+	l._log.Fatal(fmt.Sprint(args...))
+}
+
+// Fatalln logs a message at level Fatal on the compatibleLogger.
+func (l ZapLogger) Fatalln(args ...interface{}) {
+	l._log.Fatal(fmt.Sprint(args...))
+}
+
+// Fatalf logs a message at level Fatal on the compatibleLogger.
+func (l ZapLogger) Fatalf(format string, args ...interface{}) {
+	l._log.Fatal(fmt.Sprintf(format, args...))
+}
+
+// Panic logs a message at level Painc on the compatibleLogger.
+func (l ZapLogger) Panic(args ...interface{}) {
+	l._log.Panic(fmt.Sprint(args...))
+}
+
+// Panicln logs a message at level Painc on the compatibleLogger.
+func (l ZapLogger) Panicln(args ...interface{}) {
+	l._log.Panic(fmt.Sprint(args...))
+}
+
+// Panicf logs a message at level Painc on the compatibleLogger.
+func (l ZapLogger) Panicf(format string, args ...interface{}) {
+	l._log.Panic(fmt.Sprintf(format, args...))
+}
+
+// With return a logger with an extra field.
+func (l *ZapLogger) With(key string, value interface{}) *ZapLogger {
+	return &ZapLogger{l._log.With(zap.Any(key, value))}
+}
+
+// WithField return a logger with an extra field.
+func (l *ZapLogger) WithField(key string, value interface{}) *ZapLogger {
+	return &ZapLogger{l._log.With(zap.Any(key, value))}
+}
+
+// WithFields return a logger with extra fields.
+func (l *ZapLogger) WithFields(fields map[string]interface{}) *ZapLogger {
+	i := 0
+	var clog *ZapLogger
+	for k, v := range fields {
+		if i == 0 {
+			clog = l.WithField(k, v)
+		} else {
+			clog = clog.WithField(k, v)
+		}
+		i++
+	}
+	return clog
+}
+
+// FormatStdLog set the output of stand package log to zaplog
+func FormatStdLog() {
+	log.SetFlags(log.Llongfile)
+	log.SetOutput(&logWriter{NewNoCallerLogger(false)})
+}
+
+type logWriter struct {
+	logger *zap.Logger
+}
+
+// Write implement io.Writer, as std log's output
+func (w logWriter) Write(p []byte) (n int, err error) {
+	i := bytes.Index(p, []byte(":")) + 1
+	j := bytes.Index(p[i:], []byte(":")) + 1 + i
+	caller := bytes.TrimRight(p[:j], ":")
+	// find last index of /
+	i = bytes.LastIndex(caller, []byte("/"))
+	// find penultimate index of /
+	i = bytes.LastIndex(caller[:i], []byte("/"))
+	w.logger.Info("stdLog", zap.ByteString("caller", caller[i+1:]), zap.ByteString("log", bytes.TrimSpace(p[j:])))
+	n = len(p)
+	err = nil
+	return
+}
+
+var baseLogger = &ZapLogger{NewLogger(false).WithOptions(zap.AddCallerSkip(2))}
+
+// With attaches a key,value pair to a logger.
+func With(key string, value interface{}) *ZapLogger {
+	return baseLogger.With(key, value)
+}
+
+//// SetLevel sets the Level of the base logger
+//func SetLevel(level zapcore.Level) {
+//	baseLogger.SetLevel(level)
 //}
 //
 
-func (l *ZapLogger) SetFormat(format string) {
-	l.Config.Encoding = format
-	l.Config.Build()
-}
-
-// Debug logs a message at level Debug on the standard logger.
-func (l *ZapLogger) Debug(args ...interface{}) {
-	l.logger.Sugar().Debug(args...)
-}
-
-// Debugf logs a message at level Debug on the standard logger.
-func (l *ZapLogger) Debugf(format string, args ...interface{}) {
-	l.logger.Sugar().Debugf(format, args...)
-}
-
-// Debugf logs a message at level Debug on the standard logger.
-func (l *ZapLogger) Debugw(msg string, keysAndValues ...interface{}) {
-	l.logger.Sugar().Debugw(msg, keysAndValues...)
-}
-
-// Info logs a message at level Info on the standard logger.
-func (l *ZapLogger) Info(args ...interface{}) {
-	l.logger.Sugar().Info(args...)
-}
-
-// Infof logs a message at level Info on the standard logger.
-func (l *ZapLogger) Infof(format string, args ...interface{}) {
-	l.logger.Sugar().Infof(format, args...)
-}
-
-// Infow logs a message at level Info on the standard logger.
-func (l *ZapLogger) Infow(msg string, keysAndValues ...interface{}) {
-	l.logger.Sugar().Infow(msg, keysAndValues...)
-}
-
-// Warn logs a message at level Warn on the standard logger.
-func (l *ZapLogger) Warn(args ...interface{}) {
-	l.logger.Sugar().Warn(args...)
-}
-
-// Warnf logs a message at level Warn on the standard logger.
-func (l *ZapLogger) Warnf(format string, args ...interface{}) {
-	l.logger.Sugar().Warnf(format, args...)
-}
-
-// Warnw logs a message at level Warn on the standard logger.
-func (l *ZapLogger) Warnw(msg string, keysAndValues ...interface{}) {
-	l.logger.Sugar().Warnw(msg, keysAndValues...)
-}
-
-// Error logs a message at level Error on the standard logger.
-func (l *ZapLogger) Error(args ...interface{}) {
-	l.logger.Sugar().Error(args...)
-}
-
-// Errorf logs a message at level Error on the standard logger.
-func (l *ZapLogger) Errorf(format string, args ...interface{}) {
-	l.logger.Sugar().Errorf(format, args...)
-}
-
-// Errorw logs a message at level Error on the standard logger.
-func (l *ZapLogger) Errorw(msg string, keysAndValues ...interface{}) {
-	l.logger.Sugar().Errorw(msg, keysAndValues...)
-}
-
-// Fatal logs a message at level Fatal on the standard logger.
-func (l *ZapLogger) Fatal(args ...interface{}) {
-	l.logger.Sugar().Fatal(args...)
-}
-
-// Fatalf logs a message at level Fatal on the standard logger.
-func (l *ZapLogger) Fatalf(format string, args ...interface{}) {
-	l.logger.Sugar().Fatalf(format, args...)
-}
-
-// DPanic logs a message at level Panic on the standard logger.
-func (l *ZapLogger) DPanic(args ...interface{}) {
-	l.logger.Sugar().DPanic(args...)
-}
-
-// DPanicf logs a message at level Panic on the standard logger.
-func (l *ZapLogger) DPanicf(format string, args ...interface{}) {
-	l.logger.Sugar().DPanicf(format, args...)
-}
-
-// Panic logs a message at level Panic on the standard logger.
-func (l *ZapLogger) Panic(args ...interface{}) {
-	l.logger.Sugar().Panic(args...)
-}
-
-// Panicf logs a message at level Panic on the standard logger.
-func (l *ZapLogger) Panicf(format string, args ...interface{}) {
-	l.logger.Sugar().Panicf(format, args...)
-}
-
-var baseLogger, _ = NewLogger()
-
-// With attaches a key,value pair to a logger.
-func With(fields ...zapcore.Field) *ZapLogger {
-	return baseLogger.With(fields...)
-}
-
-// SetLevel sets the Level of the base logger
-func SetLevel(level zapcore.Level) {
-	baseLogger.SetLevel(level)
-}
-
-// SetFormat sets the Format of the base logger
-func SetFormat(format string) {
-	baseLogger.SetFormat(format)
-}
-
 // Debug logs a message at level Debug on the standard logger.
 func Debug(args ...interface{}) {
-	baseLogger.Debug(args...)
+	baseLogger.Debug(fmt.Sprint(args...))
 }
 
 // Debugf logs a message at level Debug on the standard logger.
 func Debugf(format string, args ...interface{}) {
-	baseLogger.Debugf(format, args...)
+	baseLogger.Debug(fmt.Sprintf(format, args...))
 }
 
 // Debugw logs a message at level Debug on the standard logger.
-func Debugw(msg string, args ...interface{}) {
-	baseLogger.Debugw(msg, args...)
+func Debugln(args ...interface{}) {
+	baseLogger.Debug(fmt.Sprint(args...))
 }
 
 // Info logs a message at level Info on the standard logger.
 func Info(args ...interface{}) {
-	baseLogger.Info(args...)
+	baseLogger.Info(fmt.Sprint(args...))
 }
 
 // Infof logs a message at level Info on the standard logger.
 func Infof(format string, args ...interface{}) {
-	baseLogger.Infof(format, args...)
+	baseLogger.Info(fmt.Sprintf(format, args...))
 }
 
 // Infow logs a message at level Info on the standard logger.
-func Infow(msg string, keysAndValues ...interface{}) {
-	baseLogger.Infow(msg, keysAndValues...)
+func Infoln(format string, args ...interface{}) {
+	baseLogger.Info(fmt.Sprintf(format, args...))
 }
 
 // Warn logs a message at level Warn on the standard logger.
 func Warn(args ...interface{}) {
-	baseLogger.Warn(args...)
+	baseLogger.Warn(fmt.Sprint(args...))
 }
 
 // Warnf logs a message at level Warn on the standard logger.
 func Warnf(format string, args ...interface{}) {
-	baseLogger.Warnf(format, args...)
+	baseLogger.Warnf(fmt.Sprintf(format, args...))
 }
 
 // Warnw logs a message at level Warn on the standard logger.
-func Warnw(msg string, keysAndValues ...interface{}) {
-	baseLogger.Warnw(msg, keysAndValues...)
+func Warnln(args ...interface{}) {
+	baseLogger.Warnln(fmt.Sprint(args...))
 }
 
 // Error logs a message at level Error on the standard logger.
 func Error(args ...interface{}) {
-	baseLogger.Error(args...)
+	baseLogger.Error(fmt.Sprint(args...))
 }
 
 // Errorf logs a message at level Error on the standard logger.
 func Errorf(format string, args ...interface{}) {
-	baseLogger.Errorf(format, args...)
+	baseLogger.Error(fmt.Sprintf(format, args...))
 }
 
-// Errorw logs a message at level Error on the standard logger.
-func Errorw(msg string, keysAndValues ...interface{}) {
-	baseLogger.Errorw(msg, keysAndValues...)
-}
-func DPanic(args ...interface{}) {
-	baseLogger.DPanic(args...)
-}
-
-// Panicln logs a message at level Fatal on the standard logger.
-func DPanicf(format string, args ...interface{}) {
-	baseLogger.DPanicf(format, args...)
+// Errorln logs a message at level Error on the standard logger.
+func Errorln(args ...interface{}) {
+	baseLogger.Error(fmt.Sprint(args...))
 }
 
 // Fatal logs a message at level Fatal on the standard logger.
 func Fatal(args ...interface{}) {
-	baseLogger.Fatal(args...)
+	baseLogger.Fatal(fmt.Sprint(args...))
 }
 
 // Fatalf logs a message at level Fatal on the standard logger.
 func Fatalf(format string, args ...interface{}) {
-	baseLogger.Fatalf(format, args...)
+	baseLogger.Fatal(fmt.Sprintf(format, args...))
+}
+
+// Fatalln logs a message at level Fatal on the standard logger.
+func Fatalln(args ...interface{}) {
+	baseLogger.Fatal(fmt.Sprint(args...))
 }
 
 // Panic logs a message at level Fatal on the standard logger.
 func Panic(args ...interface{}) {
-	baseLogger.Panic(args...)
+	baseLogger.Panic(fmt.Sprint(args...))
+}
+
+// Panicf logs a message at level Fatal on the standard logger.
+func Panicf(format string, args ...interface{}) {
+	baseLogger.Panicf(fmt.Sprintf(format, args...))
 }
 
 // Panicln logs a message at level Fatal on the standard logger.
-func Panicf(format string, args ...interface{}) {
-	baseLogger.Panicf(format, args...)
+func Panicln(args ...interface{}) {
+	baseLogger.Panic(fmt.Sprint(args...))
 }
--- logs_test.go
+++ logs_test.go
@@ -3,6 +3,7 @@ package logs
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"log"
 	"testing"
 )
 
@@ -10,13 +11,50 @@ type user struct {
 	Name string
 }
 
+func TestFormatStdLog(t *testing.T) {
+	FormatStdLog()
+	log.Print("std log")
+	log.Print("")
+}
+
+func TestNewLogger(t *testing.T) {
+	logger := NewLogger(true)
+	defer logger.Sync()
+	logger.Debug("zap log debug msg")
+}
+
+func TestNewCompatibleLogger(t *testing.T) {
+	compatibleLogger := NewCompatibleLogger(true)
+
+	compatibleLogger.WithField("field", "value").Info("compatibleLogger with info")
+	withFieldLogger := compatibleLogger.WithFields(map[string]interface{}{"field1": "value1", "field2": "value2"})
+	withFieldLogger.Info("withFieldLogger Info")
+	withFieldLogger.With("field3", "value3").Info("with filed3")
+	withFieldLogger.Debugf("withFieldLogger debugf:%v", 1)
+}
+
+func BenchmarkStdLogger(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		log.Print("std log printf")
+	}
+}
+
+func BenchmarkNewLogger(b *testing.B) {
+	logger := NewLogger(true)
+	defer logger.Sync()
+	for i := 0; i < b.N; i++ {
+		logger.Debug("zap log debug msg")
+	}
+}
 func TestNew(t *testing.T) {
-	l, _ := NewLogger()
-	l.SetFormat("console")
+	l := NewCompatibleLogger(false)
+	//l.SetFormat("console")
 	l.Info("hello world", zap.String("hello", "World"))
 	l.Warn("info logging enabled")
-	l.SetLevel(zap.InfoLevel)
+	//l.SetLevel(zap.InfoLevel)
 	l.Info("info logging disabled")
+	l.Warnf("hello  %s, %d", "world", 12)
+	l.With("ddd", "vvv").Info("dddd")
 	//time.Sleep(time.Second * 5)
 
 }
@@ -26,27 +64,19 @@ func Test(t *testing.T) {
 		&user{Name: "Zap2"},
 		&user{Name: "Zap3"},
 	}
+	Warnf("hello  %s, %d", "world", 12)
 	Debug("this is zap Debug")
 	Debugf("this is zap %s %s", "test", "zap")
 	Info("this is zap Info")
-	//SetLevel(zap.ErrorLevel)
+	////SetLevel(zap.ErrorLevel)
 	Infof("hello  %s, %d", "world", 12)
-	Errorw("hello111", "world", 12, "ttt", true)
+	Errorln("hello111", "world", 12, "ttt", true)
 	Warn("this is zap Warn")
-	Warnf("hello %s", "world", "red")
+	Warnf("hello %s,%s", "world", "red")
 	Error("this is zap Error")
 
-	//With(
-	//	"hello111111111", "world",
-	//	"failure", errors.New("oh no"),
-	//	//Stack(),
-	//	"count", 42,
-	//	"user", user{Name: "alice"},
-	//)
-
-	context := []interface{}{"foo", "bar"}
-	expectedFields := []zap.Field{zap.String("foo", "bar"), zap.Bool("baz", false)}
-	With(expectedFields...).Info(context)
+	//context := []interface{}{"foo", "bar"}
+	//expectedFields := []zap.Field{zap.String("foo", "bar"), zap.Bool("baz", false)}
 
 	Errorf("hello %s %s", "world", "red")
 
@@ -57,9 +87,9 @@ func Test(t *testing.T) {
 		return nil
 	})))
 	Fatal("this is zap Fatal")
-	Fatalf("hello %s", "world", "red")
+	//Fatalf("hello %s", "world", "red")
 	Panic("this is zap Panic")
-	Panicf("hello %d", "world", "red")
+	Panicf("hello %s ,%s", "world", "red")
 	//time.Sleep(time.Second * 5)
 
 }
